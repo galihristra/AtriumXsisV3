57a58369190303520f47a5c6f0c6db70f3043a83
irhamVS <wiganda94@gmail.com>
2016-11-23 15:45:25 +0700

    Add Form Controller by i

---
+++ AtriumXsisV2/Controllers/ListFormController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtriumXsisV2.Controllers
+{
+    public class ListFormController : Controller
+    {
+        // GET: ListForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
