5eedc8fd6d518a16b982462fd76f5d9713b7e70f
Galih Ristra <galih.ristra@gmail.com>
2016-11-23 15:05:43 +0700

    Add project files.

---
+++ AtriumXsisV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AtriumXsisV2.Startup))]
+namespace AtriumXsisV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
