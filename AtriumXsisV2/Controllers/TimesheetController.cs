0c98625a805c6395b868f3a02244fd304746c7b8
Galih Ristra <galih.ristra@gmail.com>
2016-11-23 16:00:30 +0700

    Add Timesheet Controller by Galih

---
+++ AtriumXsisV2/Controllers/TimesheetController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtriumXsisV2.Controllers
+{
+    public class TimesheetController : Controller
+    {
+        // GET: Timesheet
+        public ActionResult Index()
+        {
+            // Test
+            return View();
+        }
+    }
+}
